bc665dd9d7299290f473795dc0b83b8c9361d63f
limingan <374255041@qq.com>
2019-03-26 00:26:27 +0800

    整合swaggerUI

--- template-bg/src/main/java/com/city/management/collection/controller/PermissionController.java
+++ template-bg/src/main/java/com/city/management/collection/controller/PermissionController.java
@@ -4,6 +4,7 @@ import com.city.management.collection.model.base.Permission;
 import com.city.management.collection.service.PermissionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,9 +17,9 @@ public class PermissionController {
     @Autowired
     PermissionService permissionService;
 
-    @RequestMapping(value = "/permission/list.do")
-    public String list(HttpServletRequest request) {
+    @RequestMapping(value = "/permission/list.do",method = RequestMethod.GET)
+    public List<Permission> list(HttpServletRequest request) {
         List<Permission> permissionList = permissionService.queryAll();
-        return "login bb " + permissionList;
+        return permissionList;
     }
 }
