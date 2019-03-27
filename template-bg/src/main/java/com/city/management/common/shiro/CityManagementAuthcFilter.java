58caf5990b2f8e3e5059a7fa44d60760ccade975
GitHubCPP <751860451@qq.com>
2019-03-27 23:31:45 +0800

    shiro更新

--- template-bg/src/main/java/com/city/management/common/shiro/CityManagementAuthcFilter.java
+++ template-bg/src/main/java/com/city/management/common/shiro/CityManagementAuthcFilter.java
@@ -1,6 +1,6 @@
 package com.city.management.common.shiro;
 
-import org.apache.shiro.web.filter.authc.AuthenticationFilter;
+import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletRequest;
@@ -10,19 +10,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Component
-public class CityManagementAuthcFilter extends AuthenticationFilter{
+public class CityManagementAuthcFilter extends FormAuthenticationFilter{
 
 	@Override
 	protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
 		OutputStream os = response.getOutputStream();
 		Map<String,Object> result = new HashMap<String,Object>();
 		result.put("msg", "请先登录");
-		result.put("code", "401");
-		byte[] b = result.toString().getBytes();
+		result.put("code", "4001");
+		byte[] b = result.toString().getBytes("GBK");
 		os.write(b);
 		os.flush();
 		os.close();
-		return true;
+		return false;
 	}
 
 }
